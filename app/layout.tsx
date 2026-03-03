23900baeb39c70302894830bbeae7a899f9b6e04
Mchzks <dvillalobos@ammper.com>
2026-03-03 18:46:34 -0500

    calendars= change of name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Chronos",
+  title: "Calendars",
   description: "Visualize the flow of time through different civilizations and astronomical eras.",
 };
 
